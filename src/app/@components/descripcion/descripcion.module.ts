f1cba77ec2019435941518fea698ecf0202d6507
Pablo Scattolin <pabloscattolin04@gmail.com>
2023-03-14 17:02:57 -0300

    Proyecto Angular

---
+++ src/app/@components/descripcion/descripcion.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DescripcionComponent } from './descripcion.component';
+
+
+
+@NgModule({
+  declarations: [
+    DescripcionComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[DescripcionComponent]
+})
+export class DescripcionModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ContactModule } from './@components/contact/contact.module';
+import { CopyrightModule } from './@components/copyright/copyright.module';
+import { DescripcionModule } from './@components/descripcion/descripcion.module';
+import { HeaderModule } from './@components/header/header.module';
+import { NavbarModule } from './@components/navbar/navbar.module';
+import { PortfolioModule } from './@components/portfolio/portfolio.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContactModule,
+    CopyrightModule,
+    DescripcionModule,
+    HeaderModule,
+    NavbarModule,
+    PortfolioModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
